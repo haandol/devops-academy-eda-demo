929de9f5791b0dda4520873834aeffac3dd57f6a
haandol <ldg55d@gmail.com>
2023-03-25 15:57:00 +0900

    remove bastion

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,7 +2,6 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { VpcStack } from '../lib/stacks/vpc-stack';
-import { BastionHostStack } from '../lib/stacks/bastion-host-stack';
 import { DatabaseStack } from '../lib/stacks/database-stack';
 import { EcsClusterStack } from '../lib/stacks/ecs-cluster-stack';
 import { TripServiceStack } from '../lib/stacks/services/trip-service-stack';
@@ -24,15 +23,6 @@ new DatabaseStack(app, `${Config.app.ns}DatabaseStack`, {
 
 const vpcStack = new VpcStack(app, `${Config.app.ns}VpcStack`, {
   vpcId: Config.vpc.id,
-  env: {
-    account: Config.aws.account,
-    region: Config.aws.region,
-  },
-});
-
-new BastionHostStack(app, `${Config.app.ns}BastionHostStack`, {
-  vpcId: vpcStack.vpc.vpcId,
-  mskSecurityGroupId: Config.msk.securityGroupId,
   env: {
     account: Config.aws.account,
     region: Config.aws.region,
