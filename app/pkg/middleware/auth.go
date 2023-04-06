1be4f4921a3e6ba51530aa3b8becbe0ae16ef8a7
haandol <ldg55d@gmail.com>
2023-04-06 17:22:48 +0900

    check skippath first

--- app/pkg/middleware/auth.go
+++ app/pkg/middleware/auth.go
@@ -8,13 +8,14 @@ import (
 
 func Authencate(authHeader string, skipPaths []string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if authHeader != c.Request.Header.Get("x-auth-token") {
-			for _, path := range skipPaths {
-				if path == c.Request.URL.Path {
-					c.Next()
-					return
-				}
+		for _, path := range skipPaths {
+			if path == c.Request.URL.Path {
+				c.Next()
+				return
 			}
+		}
+
+		if authHeader != c.Request.Header.Get("x-auth-token") {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			return
 		}
