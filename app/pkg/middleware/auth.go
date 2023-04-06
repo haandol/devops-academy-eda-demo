b8c99b78503f7913d3e331dabdb99067f5c0655b
haandol <ldg55d@gmail.com>
2023-04-06 17:23:25 +0900

    revert

--- app/pkg/middleware/auth.go
+++ app/pkg/middleware/auth.go
@@ -8,14 +8,14 @@ import (
 
 func Authencate(authHeader string, skipPaths []string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		for _, path := range skipPaths {
-			if path == c.Request.URL.Path {
-				c.Next()
-				return
-			}
-		}
-
 		if authHeader != c.Request.Header.Get("x-auth-token") {
+			for _, path := range skipPaths {
+				if path == c.Request.URL.Path {
+					c.Next()
+					return
+				}
+			}
+
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			return
 		}
